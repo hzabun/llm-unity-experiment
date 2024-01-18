fe03716eb4b82e3620b6fc288340f3e07496afca
Halil Zabun <96207456+Hazab01@users.noreply.github.com>
2024-01-18 07:27:52 +0100

    feat: add camera following character

---
+++ Assets/Code/Scripts/Level one script/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // The character's Transform
+    public float smoothing = 5f; // How smoothly the camera catches up with its target movement
+
+    Vector3 offset; // The initial offset from the target
+
+    void Start()
+    {
+        // Calculate the initial offset.
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = target.position + offset;
+
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
